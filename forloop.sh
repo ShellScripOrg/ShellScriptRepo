bdb90bf53eb12a97d1ec7a9f94495d79f44794c2
ajayjamliya76 <ajayjamliya414@gmail.com>
2023-11-11 12:53:59 +0000

    1stcommit

---
+++ forloop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#Author: Ajay Jamliya
+#Date: 28/oct/2023
+#Modification: 28/oct/2023
+#Description: foor loop printing 1 to 5
+
+echo 
+for i in 1 2 3 4 5
+do
+echo value of i is $i
+done
+echo 
---
+++ forloop2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Author: Ajay Jamliya
+#Date: 28/oct/2023
+#Modification: 28/oct/2023
+#Description: foor loop printing fruits name
+
+echo 
+for i in apple mango papayya Grapes
+do
+echo see Ajay is eating $i
+done
+echo
+
