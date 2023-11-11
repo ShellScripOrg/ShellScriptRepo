d133d5870bee0a8d72b44b7744908d2619eba199
ajayjamliya76 <ajayjamliya414@gmail.com>
2023-11-11 11:31:30 +0000

    1st commit

---
+++ variables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Author: Ajay Jamliya
+#Date: 26/oct/2023
+#Modification: 26/oct/2023
+#Description: We are definig variables in this script
+
+echo
+p=pwd
+l=ls
+wi=whoami
+d=date 
+c='cal 2023'
+echo 
+echo Run variable tasks
+echo $p
+echo $l
+echo $wi 
+echo $d 
+echo $c 
+
+echo Script ended
