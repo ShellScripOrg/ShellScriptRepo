e2b080a8308450f17468ca395de5fef4e8f0932c
ajayjamliya76 <ajayjamliya414@gmail.com>
2023-11-11 11:25:36 +0000

    1stcommit

---
+++ adminTasks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Author: Ajay Jamliya
+#Date: 26/oct/2023
+#Modification: 26/oct/2023
+#Description: This script will run basic Admin tasks
+
+echo 
+top | head -10 
+echo 
+df -h 
+echo 
+uptime 
+echo 
+iostat
+echo 
+echo End for script
+
