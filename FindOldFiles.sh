2b2238349dda4aec3ec70842f2b3b01c9b5f8b60
ajayjamliya76 <ajayjamliya414@gmail.com>
2023-11-15 16:38:24 +0000

    1st commit

---
+++ FindOldFiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Author: Ajay Jamliya
+#Date: 28/oct/2023
+#Modification: 28/oct/2023
+#Description:This script will check and deelte older files
+
+find /home/ec2-user/ShellScriptPus2Git -mtime +90 -exec ls -l {} \;
+
